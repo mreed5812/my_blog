2f294d0ae5a06c1265caa66baaf2d989ef2836c8
Michael Reed <mreed5812@gmail.com>
2021-02-27 23:04:37 -0500

    Created app to handle users.

--- blog/settings.py
+++ blog/settings.py
@@ -31,8 +31,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    #myblog
+    #my apps
     'my_blog',
+    'users',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
---
+++ users/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path, include
+
+app_name = 'users'
+url_patterns = [
+    path('', include('django.contrib.auth.urls')),
+]
